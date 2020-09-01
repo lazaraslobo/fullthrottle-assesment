9fdc1ccb70cff769490a9abf2724efd4de364a14
Deepak Lobo <deepaklobo16@gmail.com>
2020-09-01 20:13:11 +0530

    builds included

---
+++ build/precache-manifest.5d4e4a3517420143d751c43749cae44b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c91533f78abcc88ca157ea891d980da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8aa51258ef6caf79d57",
+    "url": "/static/css/main.cab129ee.chunk.css"
+  },
+  {
+    "revision": "229b460fdc26dc76575a",
+    "url": "/static/js/2.fe60ad18.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.fe60ad18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8aa51258ef6caf79d57",
+    "url": "/static/js/main.5650d96a.chunk.js"
+  },
+  {
+    "revision": "f38306e161db7c73b8ca",
+    "url": "/static/js/runtime-main.8a0b1d5a.js"
+  }
+]);
