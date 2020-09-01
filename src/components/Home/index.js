c2f9298d485e6e42dc5cd9921f80a149194f251b
Deepak Lobo <deepaklobo16@gmail.com>
2020-09-01 12:59:00 +0530

    added fixes

---
+++ src/components/Home/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {FetchMockApi} from '../../core/api/api-service';
+
+class HomeComponent extends React.Component{
+    constructor(){
+        super()
+    }
+
+    render(){
+        return (
+            <h1>Home Components</h1>
+        )
+    }
+}
+
+
+export default HomeComponent
---
+++ src/core/api/api-service.js
@@ -0,0 +1,10 @@
+import MockUsersData from './mock-api';
+
+const ApiCustomize = {
+    isApiData   :   MockUsersData.members.length ? true : false,
+    data        :   MockUsersData.members || []
+}
+
+export const FetchMockApi = () =>{
+    return ApiCustomize;
+}
